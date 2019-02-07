473b374af90d48e1f8937c7bb2bc17b6e2fa63c6
JinYang0323 <jinyang0323@gmail.com>
2019-02-06 19:08:09 -0800

    Update Assignment week 3

--- src/Article.js
+++ src/Article.js
@@ -1,36 +1,17 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import Title from './Title';
-import Date from './Date';
-import Author from './Author';
+import React, { Component } from "react";
+import Title from "./Title";
 
 export default class Article extends Component {
-    constructor(props){
-        super(props);
-    }
-    render(){
-        return(
-            <div>
-                <Title title= {this.props.title} />
-                <Date date= {this.props.date} />
-                <Author authorName= {this.props.author} />
-                <div>{this.props.children}</div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Title title={this.props.title} />
+        <div align="center">
+          <date>{this.props.date}</date>
+        </div>
+        <address align="center">{this.props.author}</address>
+        <div>{this.props.children}</div>
+      </div>
+    );
+  }
 }
-
-const Article2 = ({ props }) => (
-    <div>
-        <Title title= {props.title} />
-        <Date date= {props.date} />
-        <Author authorName= {props.author} />
-        <div>{props.children}</div>
-    </div>
-);
-
-Article2.propTypes = {
-    title: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
-    author: PropTypes.string.isRequired
-};
