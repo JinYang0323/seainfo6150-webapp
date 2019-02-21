8186dfe19ef3ef440ee30c3e695cbf0d05c3ff02
JinYang0323 <jinyang0323@gmail.com>
2019-02-20 19:17:52 -0800

    Assignment-week-7

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
-import Text from "./Text";
-import ArticleTable from "./ArticleTable";
 import articles from "./data/articles.json";
 import ArticleList from "./ArticleList/ArticleList";
+import styles from "./App.module.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ArticleList articles={articles} />
+      <div class={styles.App}>
+        <div class={styles.topPanel}>
+          <p class={styles.textLeft}>The INFO6150 News</p>
+          <p class={styles.textRight}>Wed, February 20, 2019</p>
+        </div>
+        <div class={styles.articlePanel}>
+          <ArticleList articles={articles} />
+        </div>
+        <div class={styles.bottomPanel}>
+          <p>ⓒ2019, Jin Yang</p>
+        </div>
       </div>
     );
   }
